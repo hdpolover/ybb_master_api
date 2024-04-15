65b58de3b679c700a5610b6aded6aba7e5c0bcb1
Ronald Arrival Fajar <38059376+juraganpisang@users.noreply.github.com>
2024-04-15 12:09:57 +0700

    Update Participant_statuses.php

--- application/controllers/Participant_statuses.php
+++ application/controllers/Participant_statuses.php
@@ -66,6 +66,38 @@ class Participant_statuses extends RestController
         }
     }
 
+    function check_status_get()
+    {
+        $sql = $this->mCore->query_data("SELECT participants.id, participants.user_id, participants.full_name, participant_statuses.general_status 
+        FROM participants LEFT JOIN participant_statuses ON participants.id = participant_statuses.participant_id
+        WHERE general_status IS NULL")->result_array();
+
+        if ($sql) {
+            foreach ($sql as $row) {
+
+                // participant statues
+                $participant_statuses = array(
+                    'participant_id' => $row['id'],
+                    'general_status' => 0,
+                    'form_status' => 0,
+                    'document_status' => 0,
+                    'payment_status' => 0,
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                );
+                $this->mCore->save_data('participant_statuses', $participant_statuses);
+            }
+            $this->response([
+                'status' => true,
+                'data' => 'Yeay, success'
+            ], 200);
+        }
+        $this->response([
+            'status' => false,
+            'message' => 'Sorry, not found participant statuses'
+        ], 404);
+    }
+
     //SIMPAN DATA
     function save_post()
     {
@@ -120,6 +152,27 @@ class Participant_statuses extends RestController
         }
     }
 
+    //UPDATE STATUS
+    function update_status_post($id)
+    {
+        $name_col = $this->post('name');
+        $val_col = $this->post('value');
+
+        $sql = $this->mCore->save_data('participant_statuses', [$name_col => $val_col], true, ['participant_id' => $id]);
+        if ($sql) {
+            $last_data = $this->mCore->get_data('participant_statuses', ['participant_id' => $id])->row_array();
+            $this->response([
+                'status' => true,
+                'data' => $last_data
+            ], 200);
+        } else {
+            $this->response([
+                'status' => false,
+                'message' => 'Sorry, failed to update'
+            ], 404);
+        }
+    }
+
     //DELETE DATA
     function delete_get()
     {
@@ -142,5 +195,4 @@ class Participant_statuses extends RestController
             ], 404);
         }
     }
-
 }
