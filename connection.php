9a9419d8457a1b8aeea37658ad94abc652ccc6a6
Ronald Arrival Fajar <ronaldarrivalf@gmail.com>
2024-07-31 20:36:33 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,20 @@
 <?php 
-$db = mysqli_connect("localhost","root","","db_ybb");
+// ganti ke true untuk development, false untuk production
+$isDev = false;
+
+if ($isDev) {
+	$_hostname = 'localhost';
+	$_username = 'root';
+	$_password = '';
+	$_database = 'db_ybb';
+} else {
+	$_hostname = 'localhost';
+	$_username = 'u1437096_ybb_master_app_admin_user';
+	$_password = '#E1}1M^Yjs^d';
+	$_database = 'u1437096_ybb_master_app_db';
+}
+
+$db = mysqli_connect($_hostname,$_username,$_password,$_database);
  
 // Check connection
 if (mysqli_connect_errno()){
