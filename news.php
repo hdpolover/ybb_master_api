3f028cfb5e2b50202f93dea8c826f7b86d320e96
Ronald Arrival Fajar <ronaldarrivalf@gmail.com>
2024-10-08 17:03:32 +0700

    Update news.php

--- news.php
+++ news.php
@@ -15,15 +15,15 @@ if ($news_id) {
 
     $decryptedData = openssl_decrypt($encryptedData, $method, $key, $options, $iv);
 
-    $cek_program_categories = mysqli_query($db, "SELECT * FROM program_categories WHERE id = '" . $decryptedData . "'");
-    $cek_program_categories_cnt = mysqli_num_rows($cek_program_categories);
-    if ($cek_program_categories_cnt == 0) {
+    $cek_news = mysqli_query($db, "SELECT program_categories.web_url FROM program_categories JOIN programs ON program_categories.id = programs.program_category_id JOIN program_announcements ON programs.id = program_announcements.program_id WHERE program_announcements.id = '" . $decryptedData . "'");
+    $cek_news_cnt = mysqli_num_rows($cek_news);
+    if ($cek_news_cnt == 0) {
         echo '<script language="javascript">';
-        echo 'alert("Program Categories not found!")';
+        echo 'alert("News not found!")';
         echo '</script>';
     } else {
-        $url_news = mysqli_fetch_array($cek_program_categories);
-        header("Location: https://".$url_news['web_url'].'/news');
+        $url_news = mysqli_fetch_array($cek_news);
+        header("Location: https://".$url_news['web_url']."/news?id=".$encryptedData);
         die();
     }
 } else {
